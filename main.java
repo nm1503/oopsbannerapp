05de87124e047dae8ae91b57083624637ec8519f
nalin <nalinm0925@gmail.com>
2026-02-22 18:31:29 +0530

    created uc2

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
-public class main{
-    public static void main(String args[]){
-        System.out.print("OOPS");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  ***  " + " " + "   ***  " + " " + "  *****" + " " + "  ****** ");
+        System.out.println(" **  **" + " " + "  **  **" + " " + "  **  **" + " " + " **     ");
+        System.out.println("**    **" + " " + "**    **" + " " + " **  **" + " " + " **     ");
+        System.out.println("**    **" + " " + "**    **" + " " + " *****" + " " + "  ***** ");
+        System.out.println("**    **" + " " + "**    **" + " " + " **" + " " + "       **");
+        System.out.println(" **  **" + " " + "  **  **" + " " + "  **" + " " + "  **   **");
+        System.out.println("  ***  " + " " + "   ***  " + " " + "  **" + " " + "   ***** ");
     }
 }
